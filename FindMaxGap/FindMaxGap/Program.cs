a178df0808abcc282a62a2189fd1b0045acfcb3a
stKris <hslavkov@gmail.com>
2019-05-25 17:26:56 +0100

    first commit
    Find the largest gap of 0s between 1s in a binary number

---
+++ FindMaxGap/FindMaxGap/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FindMaxGap
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(FindGap(559)); // 
+        }
+
+        public static int FindGap(int number)
+        {
+            int currGap = 0;
+            int maxGap = 0;
+
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                foreach (char c in Convert.ToString(number, 2))
+                {
+                    if (c == '0')
+                    {
+                        currGap++;
+
+                        // don't reinvent the wheel 
+                        // maxGap = Math.Max(currGap, maxGap);
+                        maxGap = MaxNumber(currGap, maxGap);
+                    }
+                    else
+                    {
+                        //reset
+                        currGap = 0;
+                    }
+                }
+                return maxGap;
+            }
+        }
+        // try to reuse functionality
+        public static int MaxNumber(int num1, int num2) => num1 > num2 ? num1 : num2;
+    }
+}
