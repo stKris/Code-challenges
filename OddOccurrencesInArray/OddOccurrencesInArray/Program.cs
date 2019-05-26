e7a3689c2974fbca1790fe05917890dddc71bf6d
stKris <hslavkov@gmail.com>
2019-05-26 17:01:37 +0100

    odd occurrences in array solution
    XOR

---
+++ OddOccurrencesInArray/OddOccurrencesInArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace OddOccurrencesInArray
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] ar = new int[] { 9, 3, 9, 9, 3, 9, 6, 9 };
+
+            Console.WriteLine(Solution(ar));
+        }
+
+        public static int Solution(int[] arr)
+        {
+            int answer = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                // XOR, makes things simple
+                answer = answer ^ arr[i];
+            }
+
+            return answer;
+        }
+    }
+}
