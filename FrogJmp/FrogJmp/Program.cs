44a6b4e257d02776d338d91a1e35e83e179af697
stKris <hslavkov@gmail.com>
2019-05-31 21:53:41 +0100

    Solution
    (int)Math.Ceiling((decimal)(Y-X)/D))

---
+++ FrogJmp/FrogJmp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FrogJmp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(solution(10, 55, 10));
+
+            Console.Read();
+        }
+
+        public static int solution(int X, int Y, int D)
+        {
+            if (X == Y)
+            {
+                return 0;
+            }
+            else
+            {
+                return (int)Math.Ceiling((decimal)(Y - X) / D);
+            }
+        }
+    }
+}
